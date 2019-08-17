2303c88281da9b88771b38b81953a2e158dc3155
Aegario <denis.shelm@mail.ru>
2019-08-18 01:33:11 +0700

    minor changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,8 +21,7 @@ export const Header = ({ fetchedFilters, onFilterChange, currentFilters, onSearc
                     <LevelFilter
                         fetchedFilters={fetchedFilters}
                         onFilterChange={onFilterChange}
-                        keyGen={uuidv4}
-                        categoryFilterValue={currentFilters.level}
+                        keyGen={uuidv4} categoryFilterValue={currentFilters.level}
                     />
                     <CategoryFilter
                         fetchedFilters={fetchedFilters}
